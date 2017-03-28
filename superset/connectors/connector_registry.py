5793dd820457ccc8c08d3f89c50061f3cb8acffd
Bo Blanton <boblanton@myfitnesspal.com>
2017-03-28 08:39:57 -0700

    dumb

--- superset/connectors/connector_registry.py
+++ superset/connectors/connector_registry.py
@@ -71,5 +71,5 @@ class ConnectorRegistry(object):
     def query_datasources_by_name(
             cls, session, database, datasource_name, schema=None):
         datasource_class = ConnectorRegistry.sources[database.type]
-        return query_datasources_by_name.query_datasources_by_name(
+        return datasource_class.query_datasources_by_name(
             session, database, datasource_name, schema=None)
